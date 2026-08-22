63d8de785fb58f870208be31b3d779402d7587b3
NDRWhun <djfallen3334@gmail.com>
2026-08-22 19:50:24 +0200

    cdram ballast 24MB: coup's 21.7MB world vertex buffer outgrew the 20MB hole

--- src/vita/gxm/gxm_memory.h
+++ src/vita/gxm/gxm_memory.h
@@ -54,7 +54,7 @@ uint32_t GxmMem_FreeCdram(void);
 
 // the movie decoder allocates from phycont internally, so spill leaves it this much room
 #define GXM_PHYCONT_DECODER_RESERVE (12u * 1024u * 1024u)
-#define GXM_CDRAM_BALLAST (20u * 1024u * 1024u)
+#define GXM_CDRAM_BALLAST (24u * 1024u * 1024u)
 
 void GxmMem_BallastInit(void);
 bool GxmMem_BallastRelease(void);
